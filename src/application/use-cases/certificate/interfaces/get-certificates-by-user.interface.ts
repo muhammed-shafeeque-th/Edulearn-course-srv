bf269bb95342ea03a3919b4dcca9ae00c95bda4e
shafeeque <shafeeqsha06@gmail.com>
2026-06-23 22:24:10 +0530

    refactor(certificate): refactor course-certificate use-cases         changes:            - decoupled use-cases by introducing interface contracts            - devide use-cases into /interfaces*, /impls/* adhering to DI              priciples and impls depend on abstract interfaces

---
+++ src/application/use-cases/certificate/interfaces/get-certificates-by-user.interface.ts
@@ -0,0 +1,13 @@
+import { CertificateDto } from "src/application/dtos/certificate.dto";
+import { GetCertificatesByUserRequest } from "src/infrastructure/grpc/generated/course/types/certificate";
+
+export abstract class IGetCertificatesByUserUseCase {
+  /**
+   * Retrieves certificates by user with pagination support.
+   * @param dto - The request object containing userId and pagination.
+   * @returns An object with the list of CertificateDto and the total count.
+   */
+  abstract execute(
+    dto: GetCertificatesByUserRequest,
+  ): Promise<{ certificates: CertificateDto[]; total: number }>;
+}
