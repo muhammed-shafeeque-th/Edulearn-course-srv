fa327dd56495a3509f810c25554a2b9b2648ab96
shafeeque <shafeeqsha06@gmail.com>
2026-07-23 15:28:23 +0530

    feat(health): add health module, with check services for Kafka, Redis, Db etc

---
+++ src/infrastructure/health/checks/redis.check.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { ICacheService } from "@/application/adaptors/cache-service";
+import { HealthCheckResult,  BaseHealthCheck, HealthRegistry } from "@edulearn/nest";
+
+@Injectable()
+export class RedisHealthCheck
+  extends BaseHealthCheck
+{
+  readonly name = "redis";
+
+  constructor(
+    registry: HealthRegistry,
+    private readonly cache: ICacheService,
+  ) {
+    super(registry);
+  }
+  async check(): Promise<HealthCheckResult> {
+    try {
+      await this.cache.ping();
+      return {
+        name: "redis",
+        status: "up",
+      };
+    } catch (error: any) {
+      return {
+        name: "redis",
+        status: "down",
+        message: error?.message ?? "unknown",
+      };
+    }
+  }
+}
